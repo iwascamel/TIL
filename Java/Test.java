efa141110f65b98967acfdeac533f9a89251ecff
camel-man-ims <gudwnsrh@gmail.com>
2022-11-09 10:51:25 +0900

    서브모듈추가

--- Java/Test.java
+++ Java/Test.java
@@ -2,17 +2,5 @@ package Java;
 
 public class Test{
     public static void main(String[] args)  {
-        ThisType<Animal> animalThisType = new ThisType<>();
-    }
-
-    static class ThisType<T>{
-        T data;
-    }
-
-    static class Animal{
-
-    }
-    static class Dog extends Animal{
-
     }
 }
